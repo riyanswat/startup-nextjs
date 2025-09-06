1c1aa54068c7f7113747a274c44dc69de66f3494
Rayan Khan <riyanswat@gmail.com>
2025-09-06 19:50:11 +0900

    chore: shift nav links right

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -72,7 +72,7 @@ const Header = () => {
                 />
               </Link>
             </div>
-            <div className="flex w-full items-center justify-between px-4">
+            <div className="ml-auto flex items-center justify-end px-4">
               <div>
                 <button
                   onClick={navbarToggleHandler}
